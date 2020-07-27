7beb531f1e756bc24311e208ae69b82871f9c659
nicolai <mattrnicolai@gmail.com>
2020-07-26 23:56:55 -0400

    added pages and components

---
+++ src/Components/SearchForm/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function SearchForm(props) {
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label htmlFor="book">Book Title:</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="book"
+          list="books"
+          type="text"
+          className="form-control"
+          placeholder="Type in a book title to begin"
+          id="book"
+        />
+        <datalist id="books">
+          {props.breeds.map(breed => (
+            <option value={breed} key={breed} />
+          ))}
+        </datalist>
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
